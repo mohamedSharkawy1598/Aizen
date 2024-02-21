e81757bfb5d2964edf61db32794f518610756249
XSungX <155769696+XSungX@users.noreply.github.com>
2024-02-21 23:44:49 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,28 +21,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['201284329186'] //ur owner number
+global.ownernomer = "201284329186" //ur owner number2
+global.ownername = "𝑺𝑼𝑵𝑮 𝑿👨🏻‍💻" //ur owner name
+global.ytname = "insta: XSungX" //ur yt chanel name
+global.socialm = "Whats: Sung" //ur github or insta name
+global.location = "Egypt, Qena" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
+global.botname = "𝑨𝑰𝒁𝑬𝑵 𝑩𝑶𝑻🪽"
+global.ownernumber = '201284329186'
+global.ownername = '𝑺𝑼𝑵𝑮 𝑿👨🏻‍💻'
+global.ownerNumber = ["201284329186@s.whatsapp.net"]
+global.ownerweb = "https://chat.whatsapp.com/EoWXEQ9FzIO8EzacEBdMzX"
+global.websitex = "https://chat.whatsapp.com/EoWXEQ9FzIO8EzacEBdMzX"
+global.wagc = "https://chat.whatsapp.com/EoWXEQ9FzIO8EzacEBdMzX"
+global.themeemoji = '🪽'
+global.wm = "𝑺𝑼𝑵𝑮 𝑿"
+global.botscript = 'https://chat.whatsapp.com/EoWXEQ9FzIO8EzacEBdMzX' //script link
+global.packname = "𝑨𝑰𝒁𝑬𝑵 𝑩𝑶𝑻"
+global.author = "خذ صمبوصة\n\n"
+global.creator = "201284329186@s.whatsapp.net"
+global.prefa = ['~','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
 
@@ -61,7 +61,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //messages
 global.mess = {
-    success: 'Here you go!', 
+    success: '*『ها انتا ذا! 🧸』*', 
 }
 
 let file = require.resolve(__filename)
